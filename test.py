9fe7ab353e291d98a55224694d6a309433903e91
sungduk kang <sungduk.kang.94@gmail.com>
2016-01-14 21:24:30 -0800

    Added the test.py file

---
+++ test.py
@@ -0,0 +1,26 @@
+import urllib, urllib2, json
+
+def pretty(obj):
+	return json.dumps(obj, sort_keys=True, indent=2)
+
+def safeGet(url):
+	try:
+		return json.load(urllib2.urlopen(url))
+	except urllib2.URLError, e:
+		if hasattr(e, 'reason'):
+			print 'We failed to reach a server'
+			print 'Reason: ', e.reason
+		elif hasattr(e, 'code'):
+			print "The server couldn\'t fulfill the request."
+			print "Error code: ", e.code
+		return None 
+
+def getDownloads(query, params={}):
+	baseurl = "http://www.youtubeinmp3.com/fetch/?"
+	params['format'] = 'JSON'
+	params['video'] = query
+	url = baseurl + urllib.urlencode(params)
+	print url
+	return safeGet(url)
+
+print type(getDownloads('https://www.youtube.com/watch?v=dVUT2LoYoT8'))
